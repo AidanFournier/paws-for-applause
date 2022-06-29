3e38b142e2064d231d9a2755497c89708430c9b7
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-06-29 22:26:10 +0900

    Create schema with pet model and validations

--- server/server.js
+++ server/server.js
@@ -17,6 +17,25 @@ mongoose
 })
 .then(() => console.log('DB connectiion successful!'));
 
+const petSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A pet must have a name']
+    },
+    sex: {
+        type: String,
+        required: [true, 'A pet must have a sex']
+    },
+    age: {
+        type: String,
+        required: [true, 'A pet must have an age']
+    },
+    breed: {
+        type: String
+    }
+});
+const Pet = mongoose.model('Pet', petSchema);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App running on ${port}`);
